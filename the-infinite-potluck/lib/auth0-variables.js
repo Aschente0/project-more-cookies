1239a6bf2fc3015445c09516453ecd6b29338585
Aschente <48228100+Aschente0@users.noreply.github.com>
2020-03-19 23:21:53 -0400

    fix callback url

--- the-infinite-potluck/lib/auth0-variables.js
+++ the-infinite-potluck/lib/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
     domain: 'raspy-silence-2106.auth0.com',
     clientId: 'i-GA-tjehtVT70XBybge0A5_wKV0vcPX',
-    callbackUrl: 'https://dry-river-28713.herokuapp.com/auth/callback'
+    callbackUrl: 'https://aqueous-coast-40978.herokuapp.com/auth/callback'
   }
