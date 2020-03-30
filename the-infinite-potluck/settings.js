438710c90c643a3ca1fc6b3774a2fd236abbe722
Aschente <48228100+Aschente0@users.noreply.github.com>
2020-03-29 20:23:46 -0400

    Revert "fixing dependencies"
    This reverts commit 3dad9377c8690a5363ff2a7bff6f13aa980383c2.

--- the-infinite-potluck/settings.js
+++ the-infinite-potluck/settings.js
@@ -1,6 +1,6 @@
 
-const domain = process.env.CLIENTID || 'dev-572t65wb.auth0.com';
-const clientID = process.env.DOMAIN || 'SAyrgfFmDNO9Xtwomr9GdG3inkgCTg3L';
+const domain = process.env.CLIENTID || 'raspy-silence-2106.auth0.com';
+const clientID = process.env.DOMAIN || 'i-GA-tjehtVT70XBybge0A5_wKV0vcPX';
 
 export {
     clientID,
