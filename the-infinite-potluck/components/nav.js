18fc36e4a676e031a5651d5c006292eef28be1d0
Aschente <48228100+Aschente0@users.noreply.github.com>
2020-03-29 22:25:17 -0400

    testing auth

--- the-infinite-potluck/components/nav.js
+++ the-infinite-potluck/components/nav.js
@@ -1,26 +1,15 @@
-import React from 'react';
 import Link from 'next/link';
+import PropTypes from 'prop-types';
 
-/** login handle from https://medium.com/techintoo/setting-up-auth0-with-react-nextjs-4346c303bb5b **/
-
-export default class Nav extends React.Component {
-  render() {
-    return (
-      <nav>
-        <ul>
-          <li>
-            <Link href="/">
-              <a>Home</a>
-            </Link>
-          </li>
-          <ul>
-            <li>
-              <a id="login" >Login / Register</a>
-            </li>
-          </ul>
-        </ul>
-
-        <style jsx>{`
+const Nav = ({ isLoggedIn }) => (
+  <div>
+    <nav>
+      <ul>
+        <li><Link href="/"><a>Home</a></Link></li>
+        { isLoggedIn ? ( <li><Link href="/profile"><a>Profile</a></Link></li> ) : ( <li><Link href="/login"><a>Login</a></Link></li> ) }
+        { isLoggedIn ? ( <li><Link href="/logout"><a>Logout</a></Link></li> ) : ( '' ) }
+      </ul>
+      <style jsx>{`
       :global(body) {
         margin: 0;
         font-family: 'SF Pro Text', 'SF Pro Icons', 'Helvetica Neue', 'Helvetica',
@@ -46,7 +35,66 @@ export default class Nav extends React.Component {
         font-size: 13px;
       }
     `}</style>
-      </nav>
-    )
-  }
-}
+    </nav>
+  </div>
+)
+
+Nav.propTypes = {
+  isLoggedIn: PropTypes.bool
+};
+
+export default Nav;
+
+
+// import React from 'react';
+// import Link from 'next/link';
+
+// /** login handle from https://medium.com/techintoo/setting-up-auth0-with-react-nextjs-4346c303bb5b **/
+
+// export default class Nav extends React.Component {
+//   render() {
+//     return (
+//       <nav>
+//         <ul>
+//           <li>
+//             <Link href="/">
+//               <a>Home</a>
+//             </Link>
+//           </li>
+//           <ul>
+//             <li>
+//               <a id="login" >Login / Register</a>
+//             </li>
+//           </ul>
+//         </ul>
+
+//         <style jsx>{`
+//       :global(body) {
+//         margin: 0;
+//         font-family: 'SF Pro Text', 'SF Pro Icons', 'Helvetica Neue', 'Helvetica',
+//         'Arial', sans-serif;
+//       }
+//       nav {
+//         text-align: center;
+//       }
+//       ul {
+//         display: flex;
+//         justify-content: space-between;
+//       }
+//       nav > ul {
+//         padding: 4px 16px;
+//       }
+//       li {
+//         display: flex;
+//         padding: 6px 8px;
+//       }
+//       a {
+//         color: #067df7;
+//         text-decoration: none;
+//         font-size: 13px;
+//       }
+//     `}</style>
+//       </nav>
+//     )
+//   }
+// }
---
+++ the-infinite-potluck/settings.js
@@ -0,0 +1,8 @@
+
+const domain = process.env.CLIENTID || 'raspy-silence-2106.auth0.com';
+const clientID = process.env.DOMAIN || 'i-GA-tjehtVT70XBybge0A5_wKV0vcPX';
+
+export {
+    clientID,
+    domain
+};
