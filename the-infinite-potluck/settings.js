90e5dab5857b512f5a5a175b0b872e80a4be5802
Aschente <48228100+Aschente0@users.noreply.github.com>
2020-03-29 22:35:12 -0400

    Revert "testing auth"
    This reverts commit 18fc36e4a676e031a5651d5c006292eef28be1d0.

--- the-infinite-potluck/settings.js
+++
@@ -1,8 +0,0 @@
-
-const domain = process.env.CLIENTID || 'raspy-silence-2106.auth0.com';
-const clientID = process.env.DOMAIN || 'i-GA-tjehtVT70XBybge0A5_wKV0vcPX';
-
-export {
-    clientID,
-    domain
-};
