9fac8d4de0d9d55ac9cf43344e8f1d6bf09b4c61
Aschente <48228100+Aschente0@users.noreply.github.com>
2020-04-09 14:03:04 -0400

    test run without spoonacular api

--- the-infinite-potluck/server.js
+++ the-infinite-potluck/server.js
@@ -56,10 +56,7 @@ streamio.use((socket, next) => {
 
     let jwtToken = cookie.substring(index_beg, index_end);
     console.log("JWT TOKEN: " + jwtToken);
-
-    // let decodedToken = jwt.decode(jwtToken, { complete: true });
-    let decodedToken = jwt.decode('eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik1qaERNRUkxUmtVMU5EQkJPRUl5TmtSRE1qZ3dRVGt6UlRNMU9EQXdPVUpFUXpCRlFUbEVPUSJ9.eyJuaWNrbmFtZSI6ImNhcmxvc3l1a21pbmcubGFtIiwibmFtZSI6ImNhcmxvc3l1a21pbmcubGFtQGhvdG1haWwuY29tIiwicGljdHVyZSI6Imh0dHBzOi8vcy5ncmF2YXRhci5jb20vYXZhdGFyL2YwODc3OGU1NzhmYTQ5MzBmMGRjNmM2MzM2OTRmY2IzP3M9NDgwJnI9cGcmZD1odHRwcyUzQSUyRiUyRmNkbi5hdXRoMC5jb20lMkZhdmF0YXJzJTJGY2EucG5nIiwidXBkYXRlZF9hdCI6IjIwMjAtMDQtMDlUMTc6NTM6MDIuMDg2WiIsImVtYWlsIjoiY2FybG9zeXVrbWluZy5sYW1AaG90bWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiaXNzIjoiaHR0cHM6Ly9yYXNweS1zaWxlbmNlLTIxMDYuYXV0aDAuY29tLyIsInN1YiI6ImF1dGgwfDVlNzNhOWMxY2IyNzEyMGNjZjJmODBlYyIsImF1ZCI6ImktR0EtdGplaHRWVDcwWEJ5YmdlMEE1X3dLVjB2Y1BYIiwiaWF0IjoxNTg2NDU0NzgyLCJleHAiOjE1ODY0OTA3ODIsIm5vbmNlIjoiSmFKOXdKeG5qMXRQX2RscTlGVS5sVWhiX0xPaF9uREIifQ.e7gAfDomZr2iAIzTG3ZqwDS9a8M74hhZxsqh5XjARkz0v2nNePdWhvcDG6axpCrPOWP0VItRY-IIKxaCAUPW2ym15bv0Q72T6D0_d6y9cXKxFjtJFsEx2HHCDARXqJDv-V7G18ubSj_XM8oGn6WZ5Fkk1l-aGVFFwleDKseAQ0bsDU01hi1TEzNChz6JlmJ9eiU85e-9b7Y3gNWkro4tnWYaAYl5OuareXZSGIwzydmOHyJMg0j1fLXGSSNmdOg6HRNFqsGXDfYHzW2RtVWtFii_UUSsb7dkRHgpNbGti7a04kNK5HUTAgw6MA7khNxCpSC7cN_fBHbHWP7L91QySB', { complete: true });
-
+    let decodedToken = jwt.decode(jwtToken, { complete: true });
     let kid = decodedToken.header.kid;
     let signingKey;
     client.getSigningKey(kid, (err, key) => {
